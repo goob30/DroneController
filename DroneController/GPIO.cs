e7b866599cdae818332795d11d25c96b318f493c
goob30 <jskorokhod07@gmail.com>
2024-12-13 18:31:43 -0500

    Added joystick polling lojic and is getting in MainWindow.xaml.cs

--- DroneController/GPIO.cs
+++ DroneController/GPIO.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Device.Gpio;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
 
 namespace GPIO
 {
+
+
     public class CameraFeed
     {
         public bool feedOnline = true;
@@ -16,5 +20,51 @@ namespace GPIO
     public class CamControl
     {
 
+    }
+
+    public class JoyInput
+    {
+        private DirectInput _directInput;
+        private Joystick _joystick;
+        public string JoyStatusString = "";
+        public JoyInput()
+        {
+            _directInput = new DirectInput();
+
+            var joystickGuid = Guid.NewGuid();
+            foreach (var deviceInstance in _directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AttachedOnly))
+            {
+                joystickGuid = deviceInstance.InstanceGuid;
+                break;
+            }
+
+            if (joystickGuid == Guid.Empty)
+            {
+                JoyStatusString = "No Joystick Found";
+                return;
+            }
+
+            _joystick = new Joystick(_directInput, joystickGuid);
+
+            _joystick.Acquire();
+        }
+        public Dictionary <string, int> GetJoystickInputs()
+        {
+            if (_joystick == null)
+                return null;
+
+            _joystick.Poll();
+            var state = _joystick.GetCurrentState();
+
+            var inputs = new Dictionary<string, int>
+            {
+                { "X", state.X },
+                { "Y", state.Y },
+                { "Z", state.RotationX },
+                { "camPOV", state.PointOfViewControllers[0] },
+            };
+
+            return inputs;
+        }
     }
 }
--- DroneController/MainWindow.xaml.cs
+++ DroneController/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
 using GPIO;
 
 namespace DroneController
@@ -20,12 +21,75 @@ namespace DroneController
     public partial class MainWindow : Window
     {
         private CameraFeed _cameraFeed;
+        private JoyInput _joyInput;
+        public int camZoom = 1;
         
         public MainWindow()
         {
+
             InitializeComponent();
             _cameraFeed = new CameraFeed();
+            _joyInput = new JoyInput();
+            Task.Run(() => PollJoystick());
             UpdateCameraSignalStatus();
+        }
+
+        private async void PollJoystick()
+        {
+            DateTime lastZoomChangeTime = DateTime.MinValue;
+            int zoomChangeDelay = 160;
+
+            while (true)
+            {
+
+                try
+                {
+
+                    var inputs = _joyInput.GetJoystickInputs();
+                    if (inputs != null)
+                    {
+
+                        await Dispatcher.InvokeAsync(() =>
+                        {
+
+                            controlXLabel.Content = $"X: {inputs["X"]}";
+                            controlYLabel.Content = $"Y: {inputs["Y"]}";
+                            controlZLabel.Content = $"Z: {inputs["Z"]}";
+
+                            if(inputs.ContainsKey("camPOV") && inputs["camPOV"] != -1)
+                            {
+                                var povAngle = inputs["camPOV"];
+                                DateTime now = DateTime.Now;
+
+                                if((now - lastZoomChangeTime).TotalMilliseconds >= zoomChangeDelay)
+                                {
+                                    if (povAngle == 0)
+                                    {
+                                        camZoom = Math.Min(camZoom + 1, 10);
+                                        zoomLabel.Content = $"ZOOM {camZoom}X";
+                                        lastZoomChangeTime = now;
+                                    }
+                                    else if (povAngle == 18000)
+                                    {
+                                        camZoom = Math.Max(camZoom - 1, 1);
+                                        zoomLabel.Content = $"ZOOM {camZoom}X";
+                                        lastZoomChangeTime = now;
+                                    }
+                                }   
+                                
+                            }
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    statusLabel.Content = ($"Error polling joystick: {ex.Message}");
+
+                }
+
+                await Task.Delay(50);
+            }
         }
 
         private void UpdateCameraSignalStatus()
@@ -43,7 +107,7 @@ namespace DroneController
 
         private void btnClick(object sender, RoutedEventArgs e)
         {
-            //put shit here and dont forget it this time
+            statusLabel.Content = camZoom;
             
         }
     }
